297508d297488c781887d839c40bf7f459ef4492
jangholi <marjan.jangholi@yahoo.com>
2019-06-05 15:19:22 +0430

    initial project

---
+++ src/views/Pages/Login/Login.js
@@ -0,0 +1,108 @@
+import React, { Component } from 'react';
+import { Button, Card, CardBody, CardGroup, Col, Container, Form, Input, InputGroup, InputGroupAddon, InputGroupText, Row } from 'reactstrap';
+import { Link } from 'react-router-dom';
+
+class Login extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            series: [{
+                name: 'Gases',
+                data: [
+                    {
+                        name: 'Argon',
+                        y: 0.9,
+                        color: '#3498db'
+                    },
+                    {
+                        name: 'Nitrogen',
+                        y: 78.1,
+                        color: '#9b59b6'
+                    },
+                    {
+                        name: 'Oxygen',
+                        y: 20.9,
+                        color: '#2ecc71'
+                    },
+                    {
+                        name: 'Trace Gases',
+                        y: 0.1,
+                        color: '#f1c40f'
+                    }
+                ]
+            }]
+        }
+        this.onChange = this.onChange.bind(this);
+    }
+    onChange(e) {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+    login(){
+        this.props.history.push('/dashboard')
+        console.log('state login', this.state);
+    }
+
+    render() {
+        return (
+            <div className="app flex-row align-items-center">
+                <Container>
+                    <Row className="justify-content-center">
+                        <Col md="8">
+                            <CardGroup>
+                                <Card className="p-4 login-card">
+                                    <CardBody>
+                                        <Form>
+                                            <h1>ورود</h1>
+                                            <p className="text-muted">ورود به حساب</p>
+                                            <InputGroup className="mb-3 ltr">
+                                                <InputGroupAddon addonType="prepend">
+                                                    <InputGroupText>
+                                                        <i className="icon-user"></i>
+                                                    </InputGroupText>
+                                                </InputGroupAddon>
+                                                <Input type="text" onChange={this.onChange} name={'username'} placeholder="نام کاربری" autoComplete="username" value={this.state.username} />
+                                            </InputGroup>
+                                            <InputGroup className="mb-4 ltr">
+                                                <InputGroupAddon addonType="prepend">
+                                                    <InputGroupText>
+                                                        <i className="icon-lock"></i>
+                                                    </InputGroupText>
+                                                </InputGroupAddon>
+                                                <Input type="password" onChange={this.onChange} name={'password'} placeholder="کلمه عبور" autoComplete="current-password" value={this.state.password}/>
+                                            </InputGroup>
+                                            <Row>
+                                                <Col xs="6" className="text-right">
+                                                    <Button color="link" className="px-0">کلمه عبورتان را فراموش کرده‌اید؟</Button>
+                                                </Col>
+                                                <Col xs="6" className="text-left">
+                                                    <Button color="primary" className="px-4" onClick={this.login.bind(this)}>ورود</Button>
+                                                </Col>
+                                            </Row>
+                                        </Form>
+                                    </CardBody>
+                                </Card>
+                                <Card className="text-white bg-primary py-5 d-md-down-none register-card" style={{ width: 44 + '%' }}>
+                                    <CardBody className="text-center">
+                                        <div>
+                                            <h2>ثبت‌نام</h2>
+                                            <p>
+                                                اگر عضو خانواده بزرگ باتاب نبوده‌اید؛ همین حالا ثبت‌نام کنید.
+                                            </p>
+                                            <Button color="primary" className="mt-3 register-btn" active>
+                                                <Link to='/register'>ثبت‌نام</Link>
+                                            </Button>
+                                        </div>
+                                    </CardBody>
+                                </Card>
+                            </CardGroup>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        );
+    }
+}
+
+export default Login;
---
+++ src/views/Pages/Register/Register.js
@@ -0,0 +1,137 @@
+import React, { Component } from 'react';
+import { Button, Card, CardBody, CardFooter, Col, Container, FormGroup, Form,Label , Input, InputGroup, InputGroupAddon, InputGroupText, Row } from 'reactstrap';
+import { Link } from 'react-router-dom';
+
+class Register extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            username: '',
+            email: '',
+            gender: '',
+            history: '',
+            historyFrom: '',
+            historyTo: '',
+            height: '',
+            weight: ''
+        }
+        this.onChange = this.onChange.bind(this);
+    }
+    onChange(e) {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+    register(){
+        console.log('this state:  ', this.state)
+    }
+    render() {
+        return (
+            <div className="app flex-row align-items-center">
+                <Container>
+                    <Row className="justify-content-center">
+                        <Col md="6">
+                            <Card className="mx-4">
+                                <CardBody className="p-4">
+                                    <Form>
+                                        <h1>ثبت‌نام</h1>
+                                        <p className="text-muted">حساب خود را ایجاد کنید.</p>
+                                        <InputGroup className="mb-3 ltr">
+                                            <InputGroupAddon addonType="prepend">
+                                                <InputGroupText>
+                                                    <i className="icon-user"></i>
+                                                </InputGroupText>
+                                            </InputGroupAddon>
+                                            <Input type="text" onChange={this.onChange} name={'username'} placeholder="نام کاربری" autoComplete="username" />
+                                        </InputGroup>
+                                        <InputGroup className="mb-3 ltr">
+                                            <InputGroupAddon addonType="prepend">
+                                                <InputGroupText>
+                                                    <i className="fa fa-envelope-o"></i>
+                                                </InputGroupText>
+                                            </InputGroupAddon>
+                                            <Input type="email" onChange={this.onChange} name={'email'} placeholder="ایمیل" autoComplete="email" />
+                                        </InputGroup>
+                                        <FormGroup inline>
+                                            <label className="text-muted">جنسیت:</label>
+                                            <FormGroup check inline>
+                                                <Label check>
+                                                    <Input onChange={this.onChange} name={'gender'} type="radio" name="gender" value={'male'}/>
+                                                    <span className='mr-1'>مرد</span>
+                                                </Label>
+                                            </FormGroup>
+                                            <FormGroup check inline>
+                                                <Label check>
+                                                    <Input onChange={this.onChange} name={'gender'} type="radio" name="gender" value={'female'}/>
+                                                    <span className='mr-1'>زن</span>
+                                                </Label>
+                                            </FormGroup>
+                                        </FormGroup>
+                                        <FormGroup inline >
+                                            <label className="text-muted">سابق تمرین:</label>
+                                            <FormGroup check inline>
+                                                <Label check>
+                                                    <Input type="radio" onChange={this.onChange} name={'history'} value={'yes'}/>
+                                                    <span className='mr-1'>بله</span>
+                                                </Label>
+                                            </FormGroup>
+                                            <FormGroup check inline>
+                                                <Label check>
+                                                    <Input type="radio" onChange={this.onChange} name={'history'} value={'no'} />
+                                                    <span className='mr-1'>خیر</span>
+                                                </Label>
+                                            </FormGroup>
+                                        </FormGroup>
+                                        <Row>
+                                            <InputGroup className="mb-3 ltr col-6">
+                                                <InputGroupAddon addonType="prepend">
+                                                    <InputGroupText>
+                                                        <i className="fa fa-heart-o"></i>
+                                                    </InputGroupText>
+                                                </InputGroupAddon>
+                                                <Input type="number" onChange={this.onChange} name={'historyFrom'} placeholder="از" autoComplete="height" />
+                                            </InputGroup>
+                                            <InputGroup className="mb-3 ltr col-6">
+                                                <InputGroupAddon addonType="prepend">
+                                                    <InputGroupText>
+                                                        <i className="fa fa-heart-o"></i>
+                                                    </InputGroupText>
+                                                </InputGroupAddon>
+                                                <Input type="number" onChange={this.onChange} name={'historyTo'} placeholder="تا" autoComplete="height" />
+                                            </InputGroup>
+                                        </Row>
+                                        <Row>
+                                            <InputGroup className="mb-3 ltr col-6">
+                                                <InputGroupAddon addonType="prepend">
+                                                    <InputGroupText>
+                                                        <i className="fa fa-heart-o"></i>
+                                                    </InputGroupText>
+                                                </InputGroupAddon>
+                                                <Input type="number" placeholder="قد" onChange={this.onChange} name={'height'} autoComplete="height" />
+                                            </InputGroup>
+                                            <InputGroup className="mb-3 ltr col-6">
+                                                <InputGroupAddon addonType="prepend">
+                                                    <InputGroupText>
+                                                        <i className="fa fa-heart-o"></i>
+                                                    </InputGroupText>
+                                                </InputGroupAddon>
+                                                <Input type="number" onChange={this.onChange} name={'weight'} placeholder="وزن" autoComplete="weight" />
+                                            </InputGroup>
+                                        </Row>
+                                        <Button color="success" block onClick={this.register.bind(this)}>ایجاد حساب</Button>
+                                        <p className="text-center font12 mt-15">
+                                            <span>ثبت‌نام کرده‌اید؟</span>
+                                            <Link to='/login'>&nbsp;ورود&nbsp;</Link>
+                                        </p>
+                                    </Form>
+                                </CardBody>
+                            </Card>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        );
+    }
+}
+
+export default Register;
