cb587e4a9b9e7bced8b63d0a2bad5e33ab4b0b63
jangholi <marjan.jangholi@yahoo.com>
2019-08-19 20:12:07 +0430

    delete coreui

--- src/containers/DefaultLayout/DefaultAside.js
+++ src/containers/DefaultLayout/DefaultAside.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Nav, NavItem, NavLink, Progress, TabContent, TabPane, ListGroup, ListGroupItem } from 'reactstrap';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import { AppSwitch } from '@coreui/react'
 
 const propTypes = {
   children: PropTypes.node,
